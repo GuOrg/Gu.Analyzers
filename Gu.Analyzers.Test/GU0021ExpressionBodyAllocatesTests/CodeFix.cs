02f2c47f767b21272ddc1ab623c6d570fea650b3
Johan Larsson <johan_gavle1@hotmail.com>
2016-11-30 00:11:45 +0100

    Fix formatting in tests.

--- Gu.Analyzers.Test/GU0021ExpressionBodyAllocatesTests/CodeFix.cs
+++ Gu.Analyzers.Test/GU0021ExpressionBodyAllocatesTests/CodeFix.cs
@@ -10,26 +10,26 @@
         public async Task AllocatingReferenceTypeFromImmutableMembers()
         {
             var testCode = @"
-    public class Foo
+public class Foo
+{
+    public Foo(int a, int b, int c, int d)
     {
-        public Foo(int a, int b, int c, int d)
-        {
-            this.A = a;
-            this.B = b;
-            this.C = c;
-            this.D = d;
-        }
+        this.A = a;
+        this.B = b;
+        this.C = c;
+        this.D = d;
+    }
 
-        public int A { get; }
+    public int A { get; }
 
-        public int B { get; }
+    public int B { get; }
 
-        public int C { get; }
+    public int C { get; }
 
-        public int D { get; }
+    public int D { get; }
 
-        public Foo Bar ↓=> new Foo(this.A, this.B, this.C, this.D);
-    }";
+    public Foo Bar ↓=> new Foo(this.A, this.B, this.C, this.D);
+}";
             var expected = this.CSharpDiagnostic()
                                .WithLocationIndicated(ref testCode)
                                .WithMessage("Expression body allocates reference type.");
@@ -37,28 +37,98 @@
             await this.VerifyCSharpDiagnosticAsync(testCode, new[] { expected }, CancellationToken.None).ConfigureAwait(false);
 
             var fixedCode = @"
-    public class Foo
+public class Foo
+{
+    public Foo(int a, int b, int c, int d)
     {
-        public Foo(int a, int b, int c, int d)
-        {
-            this.A = a;
-            this.B = b;
-            this.C = c;
-            this.D = d;
+        this.A = a;
+        this.B = b;
+        this.C = c;
+        this.D = d;
         this.Bar = new Foo(this.A, this.B, this.C, this.D);
     }
 
-        public int A { get; }
+    public int A { get; }
 
-        public int B { get; }
+    public int B { get; }
 
-        public int C { get; }
+    public int C { get; }
 
-        public int D { get; }
+    public int D { get; }
 
-        public Foo Bar { get; }
+    public Foo Bar { get; }
 }";
             await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task AllocatingReferenceTypeEmptyCtor()
+        {
+            var testCode = @"
+namespace Test
+{
+    public class Foo
+    {
+        public Foo()
+        {
+        }
+
+        public Foo Bar ↓=> new Foo();
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Expression body allocates reference type.");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, new[] { expected }, CancellationToken.None).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace Test
+{
+    public class Foo
+    {
+        public Foo()
+        {
+            this.Bar = new Foo();
+        }
+
+        public Foo Bar { get; }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task AllocatingReferenceTypeFromMutableMembersNoFix()
+        {
+            var testCode = @"
+public class Foo
+{
+    public Foo(int a, int b, int c, int d)
+    {
+        this.A = a;
+        this.B = b;
+        this.C = c;
+        this.D = d;
+    }
+
+    public int A { get; }
+
+    public int B { get; }
+
+    public int C { get; }
+
+    public int D { get; set; }
+
+    public Foo Bar ↓=> new Foo(this.A, this.B, this.C, this.D);
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Expression body allocates reference type.");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, new[] { expected }, CancellationToken.None).ConfigureAwait(false);
+
+            await this.VerifyCSharpFixAsync(testCode, testCode).ConfigureAwait(false);
         }
     }
 }
