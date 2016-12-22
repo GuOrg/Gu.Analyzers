d26cc3d1c1f241840d34d7f7376d8b78add6fa76
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-22 11:03:40 +0100

    wip

---
+++ Gu.Analyzers.Analyzers/GU0022UseGetOnly.cs
@@ -0,0 +1,51 @@
+﻿namespace Gu.Analyzers
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class GU0022UseGetOnly : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "GU0022";
+        private const string Title = "Use get-only.";
+        private const string MessageFormat = "Use get-only.";
+        private const string Description = "Use get-only.";
+        private static readonly string HelpLink = Analyzers.HelpLink.ForId(DiagnosticId);
+
+        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            id: DiagnosticId,
+            title: Title,
+            messageFormat: MessageFormat,
+            category: AnalyzerCategory.Correctness,
+            defaultSeverity: DiagnosticSeverity.Hidden,
+            isEnabledByDefault: AnalyzerConstants.EnabledByDefault,
+            description: Description,
+            helpLinkUri: HelpLink);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(HandleProperty, SyntaxKind.PropertyDeclaration);
+        }
+
+        private static void HandleProperty(SyntaxNodeAnalysisContext context)
+        {
+            if (context.SemanticModel == null ||
+                context.Node == null ||
+                context.Node.IsMissing)
+            {
+                return;
+            }
+
+            var propertySymbol = context.ContainingSymbol as IPropertySymbol;
+        }
+    }
+}
