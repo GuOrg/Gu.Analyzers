9140e06847ce4a39c3b1489d429d5a5c62651052
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-24 10:13:38 +0100

    Bump version.

--- Gu.Analyzers.Analyzers/Properties/AssemblyInfo.cs
+++ Gu.Analyzers.Analyzers/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.10.4")]
-[assembly: AssemblyFileVersion("0.1.10.4")]
-[assembly: AssemblyInformationalVersion("0.1.10.4-dev")]
+[assembly: AssemblyVersion("0.1.11.0")]
+[assembly: AssemblyFileVersion("0.1.11.0")]
+[assembly: AssemblyInformationalVersion("0.1.11.0-dev")]
 
 [assembly: InternalsVisibleTo("Gu.Analyzers.CodeFixes")]
 [assembly: InternalsVisibleTo("Gu.Analyzers.Test")]
