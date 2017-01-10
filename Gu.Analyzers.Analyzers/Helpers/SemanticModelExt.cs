130067a4470e4dff3cea1890d0f6b27526e8ee80
Johan Larsson <johan_gavle1@hotmail.com>
2017-01-10 12:24:17 +0100

    Find semantic models in all compilations.

--- Gu.Analyzers.Analyzers/Helpers/SemanticModelExt.cs
+++ Gu.Analyzers.Analyzers/Helpers/SemanticModelExt.cs
@@ -83,9 +83,29 @@
                 return null;
             }
 
-            return ReferenceEquals(semanticModel.SyntaxTree, expression.SyntaxTree)
-                ? semanticModel
-                : semanticModel.Compilation.GetSemanticModel(expression.SyntaxTree);
+            if (ReferenceEquals(semanticModel.SyntaxTree, expression.SyntaxTree))
+            {
+                return semanticModel;
+            }
+
+            if (semanticModel.Compilation.ContainsSyntaxTree(expression.SyntaxTree))
+            {
+                return semanticModel.Compilation.GetSemanticModel(expression.SyntaxTree);
+            }
+
+            foreach (var metadataReference in semanticModel.Compilation.References)
+            {
+                var compilationReference = metadataReference as CompilationReference;
+                if (compilationReference != null)
+                {
+                    if (compilationReference.Compilation.ContainsSyntaxTree(expression.SyntaxTree))
+                    {
+                        return compilationReference.Compilation.GetSemanticModel(expression.SyntaxTree);
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
