3f23756950a1c8e8031b9db73e14398e265791d7
Johan Larsson <johan_gavle1@hotmail.com>
2017-02-01 14:20:25 +0100

    Refactor.

--- Gu.Analyzers.Test/GU0011DontIgnoreReturnValueTests/HappyPath.cs
+++ Gu.Analyzers.Test/GU0011DontIgnoreReturnValueTests/HappyPath.cs
@@ -3,7 +3,7 @@ namespace Gu.Analyzers.Test.GU0011DontIgnoreReturnValueTests
     using System.Threading.Tasks;
     using NUnit.Framework;
 
-    internal class HappyPath : HappyPathVerifier<GU0011DontIgnoreReturnValue>
+    internal partial class HappyPath : HappyPathVerifier<GU0011DontIgnoreReturnValue>
     {
         [Test]
         public async Task ChainedCtor()
@@ -304,101 +304,6 @@ public class Foo
 }";
 
             await this.VerifyHappyPathAsync(testCode)
-                      .ConfigureAwait(false);
-        }
-
-        [Test]
-        public async Task StringBuilderAppendLine()
-        {
-            var testCode = @"
-using System.Text;
-public class Foo
-{
-    public void Bar()
-    {
-        var sb = new StringBuilder();
-        sb.AppendLine(""test"");
-    }
-}";
-            await this.VerifyHappyPathAsync(testCode)
-                      .ConfigureAwait(false);
-        }
-
-        [Test]
-        public async Task StringBuilderAppend()
-        {
-            var testCode = @"
-using System.Text;
-public class Foo
-{
-    public void Bar()
-    {
-        var sb = new StringBuilder();
-        sb.Append(""test"");
-    }
-}";
-            await this.VerifyHappyPathAsync(testCode)
-                      .ConfigureAwait(false);
-        }
-
-        [Test]
-        public async Task StringBuilderAppendChained()
-        {
-            var testCode = @"
-using System.Text;
-public class Foo
-{
-    public void Bar()
-    {
-        var sb = new StringBuilder();
-        sb.Append(""1"").Append(""2"");
-    }
-}";
-            await this.VerifyHappyPathAsync(testCode)
-                      .ConfigureAwait(false);
-        }
-
-        [Test]
-        public async Task WhenReturningThis()
-        {
-            var testCode = @"
-public class Foo
-{
-    public Foo Bar()
-    {
-        return this;
-    }
-
-    public void Meh()
-    {
-        Bar();
-    }
-}";
-            await this.VerifyHappyPathAsync(testCode)
-                      .ConfigureAwait(false);
-        }
-
-        [Test]
-        public async Task WhenExtensionMethodReturningThis()
-        {
-            var barCode = @"
-internal static class Bar
-{
-    internal static T Id<T>(this T value)
-    {
-        return value;
-    }
-}";
-            var testCode = @"
-public class Foo
-{
-    private Foo()
-    {
-        var meh =1;
-        meh.Id();
-    }
-}";
-            await this.VerifyHappyPathAsync(barCode, testCode)
                       .ConfigureAwait(false);
         }
 
