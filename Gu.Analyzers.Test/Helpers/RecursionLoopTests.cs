25b5107b9437535597d3150f65dd015f54ee6879
Johan Larsson <johan_gavle1@hotmail.com>
2017-03-03 08:43:11 +0100

    Formatting.

--- Gu.Analyzers.Test/Helpers/RecursionLoopTests.cs
+++ Gu.Analyzers.Test/Helpers/RecursionLoopTests.cs
@@ -8,47 +8,47 @@
         [Test]
         public void OneItem()
         {
-            var one = SyntaxFactory.IdentifierName("1");
-            var two = SyntaxFactory.IdentifierName("2");
+            var a = SyntaxFactory.IdentifierName("a");
+            var b = SyntaxFactory.IdentifierName("b");
             var loop = new RecursionLoop();
-            Assert.AreEqual(true, loop.Add(one));
-            Assert.AreEqual(false, loop.Add(one));
-            Assert.AreEqual(false, loop.Add(one));
-            Assert.AreEqual(true, loop.Add(two));
-            Assert.AreEqual(false, loop.Add(two));
-            Assert.AreEqual(false, loop.Add(two));
+            Assert.AreEqual(true,  loop.Add(a));
+            Assert.AreEqual(false, loop.Add(a));
+            Assert.AreEqual(false, loop.Add(a));
+            Assert.AreEqual(true,  loop.Add(b));
+            Assert.AreEqual(false, loop.Add(b));
+            Assert.AreEqual(false, loop.Add(b));
         }
 
         [Test]
         public void TwoItems()
         {
-            var one = SyntaxFactory.IdentifierName("1");
-            var two = SyntaxFactory.IdentifierName("2");
+            var a = SyntaxFactory.IdentifierName("a");
+            var b = SyntaxFactory.IdentifierName("b");
             var loop = new RecursionLoop();
-            Assert.AreEqual(true, loop.Add(one));
-            Assert.AreEqual(true, loop.Add(two));
-            Assert.AreEqual(true, loop.Add(one));
-            Assert.AreEqual(false, loop.Add(two));
-            Assert.AreEqual(false, loop.Add(one));
-            Assert.AreEqual(false, loop.Add(one));
-            Assert.AreEqual(true, loop.Add(two));
-            Assert.AreEqual(false, loop.Add(two));
+            Assert.AreEqual(true,  loop.Add(a));
+            Assert.AreEqual(true,  loop.Add(b));
+            Assert.AreEqual(true,  loop.Add(a));
+            Assert.AreEqual(false, loop.Add(b));
+            Assert.AreEqual(false, loop.Add(a));
+            Assert.AreEqual(false, loop.Add(a));
+            Assert.AreEqual(true,  loop.Add(b));
+            Assert.AreEqual(false, loop.Add(b));
         }
 
         [Test]
         public void ThreeItems()
         {
-            var one = SyntaxFactory.IdentifierName("1");
-            var two = SyntaxFactory.IdentifierName("2");
-            var three = SyntaxFactory.IdentifierName("3");
+            var a = SyntaxFactory.IdentifierName("a");
+            var b = SyntaxFactory.IdentifierName("b");
+            var c = SyntaxFactory.IdentifierName("c");
             var loop = new RecursionLoop();
-            Assert.AreEqual(true, loop.Add(one));
-            Assert.AreEqual(false, loop.Add(one));
-            Assert.AreEqual(true, loop.Add(two));
-            Assert.AreEqual(true, loop.Add(three));
-            Assert.AreEqual(true, loop.Add(one));
-            Assert.AreEqual(true, loop.Add(two));
-            Assert.AreEqual(false, loop.Add(three));
+            Assert.AreEqual(true,  loop.Add(a));
+            Assert.AreEqual(false, loop.Add(a));
+            Assert.AreEqual(true,  loop.Add(b));
+            Assert.AreEqual(true,  loop.Add(c));
+            Assert.AreEqual(true,  loop.Add(a));
+            Assert.AreEqual(true,  loop.Add(b));
+            Assert.AreEqual(false, loop.Add(c));
         }
     }
 }
