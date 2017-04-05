0a1d5a9c07a0a449748e83bf56475e63a63d8c57
milleniumbug <wojas1222@gmail.com>
2017-04-05 21:30:57 +0200

    Add more tests

--- Gu.Analyzers.Test/GU0051XmlSerializerNotCached/Diagnostics.cs
+++ Gu.Analyzers.Test/GU0051XmlSerializerNotCached/Diagnostics.cs
@@ -32,7 +32,7 @@ public class Foo
         }
 
         [TestCase(@"new XmlSerializer(typeof(Foo), new XmlRootAttribute(""rootNode""))")]
-        public async Task LanguageConstructs(string code)
+        public async Task LocalVariable(string code)
         {
             var testCode = @"
 using System;
@@ -46,6 +46,34 @@ public class Foo
         for(int i = 0; i < 100; ++i)
         {
             ↓XmlSerializer serializer = default(XmlSerializer);
+        }
+    }
+}";
+            testCode = testCode.AssertReplace("default(XmlSerializer)", code);
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("The serializer is not cached.");
+            await this.VerifyCSharpDiagnosticAsync(new[] { testCode }, expected)
+                      .ConfigureAwait(false);
+        }
+
+        [TestCase(@"new XmlSerializer(typeof(Foo), new XmlRootAttribute(""rootNode""))")]
+        public async Task LanguageConstructs(string code)
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    private static XmlSerializer serializer;
+    
+    public Foo(int a, int b, int c, int d)
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            ↓serializer = default(XmlSerializer);
         }
     }
 }";
--- Gu.Analyzers.Test/GU0051XmlSerializerNotCached/HappyPath.cs
+++ Gu.Analyzers.Test/GU0051XmlSerializerNotCached/HappyPath.cs
@@ -29,7 +29,60 @@ public class Foo
         }
 
         [Test]
-        public async Task CachedXmlSerializer()
+        public async Task CachedStaticReadonlyInitializedInlineXmlSerializer()
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    private static readonly XmlSerializer serializer = new XmlSerializer(typeof(Foo), new XmlRootAttribute(""rootNode""));
+
+    public Foo(int a, int b, int c, int d)
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            
+        }
+    }
+}";
+            await this.VerifyHappyPathAsync(testCode)
+                      .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task CachedStaticReadonlyInitializedInStaticConstructorXmlSerializer()
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    private static readonly XmlSerializer serializer;
+
+    static Foo()
+    {
+        serializer = new XmlSerializer(typeof(Foo), new XmlRootAttribute(""rootNode""));
+    }
+
+    public Foo(int a, int b, int c, int d)
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            
+        }
+    }
+}";
+            await this.VerifyHappyPathAsync(testCode)
+                      .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task CachedStaticInitializedInlineXmlSerializer()
         {
             var testCode = @"
 using System;
