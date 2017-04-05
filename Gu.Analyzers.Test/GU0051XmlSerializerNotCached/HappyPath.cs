bed732285ab07b1a92a7e2aeb637ff8829658b4f
milleniumbug <wojas1222@gmail.com>
2017-04-05 02:08:38 +0200

    Add a test case for the non-leaky constructor

--- Gu.Analyzers.Test/GU0051XmlSerializerNotCached/HappyPath.cs
+++ Gu.Analyzers.Test/GU0051XmlSerializerNotCached/HappyPath.cs
@@ -51,5 +51,29 @@ public class Foo
             await this.VerifyHappyPathAsync(testCode)
                       .ConfigureAwait(false);
         }
+
+        [TestCase(@"new XmlSerializer(typeof(Foo), ""rootNode"")")]
+        [TestCase(@"new XmlSerializer(typeof(Foo))")]
+        public async Task NonLeakyConstructor(string code)
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    public Foo(int a, int b, int c, int d)
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            XmlSerializer serializer = default(XmlSerializer);
+        }
+    }
+}";
+            testCode = testCode.AssertReplace("default(XmlSerializer)", code);
+            await this.VerifyHappyPathAsync(testCode)
+                      .ConfigureAwait(false);
+        }
     }
 }
