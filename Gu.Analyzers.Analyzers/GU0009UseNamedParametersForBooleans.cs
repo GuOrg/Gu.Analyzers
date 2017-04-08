84e408f495658e2a3a31dabeec748cd5d097095d
milleniumbug <wojas1222@gmail.com>
2017-04-08 18:00:59 +0200

    Added tests for the new analyzer

---
+++ Gu.Analyzers.Analyzers/GU0009UseNamedParametersForBooleans.cs
@@ -0,0 +1,52 @@
+﻿namespace Gu.Analyzers
+{
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Threading;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class GU0009UseNamedParametersForBooleans : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "GU0009";
+        private const string Title = "Name the boolean parameter.";
+        private const string MessageFormat = "The boolean parameter is not named.";
+        private const string Description = "The unnamed boolean parameters aren't obvious about their purpose. Consider naming the boolean argument for clarity.";
+        private static readonly string HelpLink = Analyzers.HelpLink.ForId(DiagnosticId);
+
+        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+                                                                      id: DiagnosticId,
+                                                                      title: Title,
+                                                                      messageFormat: MessageFormat,
+                                                                      category: AnalyzerCategory.Correctness,
+                                                                      defaultSeverity: DiagnosticSeverity.Hidden,
+                                                                      isEnabledByDefault: AnalyzerConstants.EnabledByDefault,
+                                                                      description: Description,
+                                                                      helpLinkUri: HelpLink);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(HandleArguments, SyntaxKind.ArgumentList);
+        }
+
+        private static void HandleArguments(SyntaxNodeAnalysisContext context)
+        {
+            if (context.IsExcludedFromAnalysis())
+            {
+                return;
+            }
+
+            var argumentListSyntax = (ArgumentListSyntax)context.Node;
+        }
+    }
+}
---
+++ Gu.Analyzers.Test/GU0009UseNamedParametersForBooleansTests/Diagnostics.cs
@@ -0,0 +1,36 @@
+﻿namespace Gu.Analyzers.Test.GU0009UseNamedParametersForBooleansTests
+{
+    using System.Threading.Tasks;
+
+    using NUnit.Framework;
+
+    internal class Diagnostics : DiagnosticVerifier<Analyzers.GU0009UseNamedParametersForBooleans>
+    {
+        [Test]
+        public async Task UnnamedBooleanParameters()
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    public void Floof(int howMuch, bool useFluffyBuns)
+    {
+        
+    }
+
+    public void Another()
+    {
+        Floof(42, ↓false);
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("The boolean parameter is not named.");
+            await this.VerifyCSharpDiagnosticAsync(new[] { testCode }, expected)
+                      .ConfigureAwait(false);
+        }
+    }
+}
