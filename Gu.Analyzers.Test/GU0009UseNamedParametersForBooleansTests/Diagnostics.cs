d6b304d71aad4caeef1e6bd82f0f29bc52bdaa2e
milleniumbug <wojas1222@gmail.com>
2017-04-08 22:16:40 +0200

    Test for different named for bool

--- Gu.Analyzers.Test/GU0009UseNamedParametersForBooleansTests/Diagnostics.cs
+++ Gu.Analyzers.Test/GU0009UseNamedParametersForBooleansTests/Diagnostics.cs
@@ -32,5 +32,60 @@ public class Foo
             await this.VerifyCSharpDiagnosticAsync(new[] { testCode }, expected)
                       .ConfigureAwait(false);
         }
+
+        [Test]
+        public async Task HandlesAnAlias()
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Alias = System.Boolean;
+
+public class Foo
+{
+    public void Floof(int howMuch, Alias useFluffyBuns)
+    {
+        
+    }
+
+    public void Another()
+    {
+        Floof(42, ↓false);
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("The boolean parameter is not named.");
+            await this.VerifyCSharpDiagnosticAsync(new[] { testCode }, expected)
+                      .ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task HandlesAFullyQualifiedName()
+        {
+            var testCode = @"
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Foo
+{
+    public void Floof(int howMuch, System.Boolean useFluffyBuns)
+    {
+        
+    }
+
+    public void Another()
+    {
+        Floof(42, ↓false);
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("The boolean parameter is not named.");
+            await this.VerifyCSharpDiagnosticAsync(new[] { testCode }, expected)
+                      .ConfigureAwait(false);
+        }
     }
 }
