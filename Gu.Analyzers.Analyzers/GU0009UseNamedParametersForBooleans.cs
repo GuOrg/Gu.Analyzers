b5a9dfe58ca04e725af82dfc6434dce65a9c0dda
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-09 00:46:44 +0200

    Remove unused.

--- Gu.Analyzers.Analyzers/GU0009UseNamedParametersForBooleans.cs
+++ Gu.Analyzers.Analyzers/GU0009UseNamedParametersForBooleans.cs
@@ -1,9 +1,6 @@
 ﻿namespace Gu.Analyzers
 {
     using System.Collections.Immutable;
-    using System.Linq;
-    using System.Threading;
-
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
