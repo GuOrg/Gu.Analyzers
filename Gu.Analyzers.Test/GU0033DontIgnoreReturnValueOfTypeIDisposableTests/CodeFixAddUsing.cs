992aef937e783805ab88e5b648aedeb00599024a
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-30 22:08:59 +0200

    More tests with poor formatting.

--- Gu.Analyzers.Test/GU0033DontIgnoreReturnValueOfTypeIDisposableTests/CodeFixAddUsing.cs
+++ Gu.Analyzers.Test/GU0033DontIgnoreReturnValueOfTypeIDisposableTests/CodeFixAddUsing.cs
@@ -41,5 +41,98 @@ public sealed class Foo
 }";
             await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
         }
+
+        [Test]
+        public async Task AddUsingForIgnoredReturnEmpty()
+        {
+            var testCode = @"
+using System;
+using System.IO;
+
+public sealed class Foo
+{
+    public void Meh()
+    {
+        ↓File.OpenRead(string.Empty);
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Don't ignore returnvalue of type IDisposable.");
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+using System;
+using System.IO;
+
+public sealed class Foo
+{
+    public void Meh()
+    {
+        using (File.OpenRead(string.Empty))
+        {
+        }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task AddUsingForIgnoredReturnManyStatements()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class Foo
+    {
+        public void Meh()
+        {
+            ↓File.OpenRead(string.Empty);
+            var a = 1;
+            var b = 2;
+            if (a == b)
+            {
+                var c = 3;
+            }
+
+            var d = 4;
+        }
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Don't ignore returnvalue of type IDisposable.");
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class Foo
+    {
+        public void Meh()
+        {
+            using (File.OpenRead(string.Empty))
+            {
+                var a = 1;
+            var b = 2;
+            if (a == b)
+            {
+                var c = 3;
+            }
+
+            var d = 4;
+            }
+        }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+            Assert.Inconclusive("poor formatting");
+        }
     }
 }
