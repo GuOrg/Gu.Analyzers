f638284aa92e607cccb670c891488d0a30be7aa4
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-30 17:27:17 +0200

    WIP Failing test for append to composite disposable.

--- Gu.Analyzers.CodeFixes/AddToCompositeDisposableCodeFixProvider.cs
+++ Gu.Analyzers.CodeFixes/AddToCompositeDisposableCodeFixProvider.cs
@@ -2,6 +2,7 @@ namespace Gu.Analyzers
 {
     using System.Collections.Immutable;
     using System.Composition;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
@@ -15,6 +16,8 @@ namespace Gu.Analyzers
     [Shared]
     internal class AddToCompositeDisposableCodeFixProvider : CodeFixProvider
     {
+        private static readonly TypeSyntax CompositeDisposableType = SyntaxFactory.ParseTypeName("CompositeDisposable");
+
         /// <inheritdoc/>
         public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(GU0033DontIgnoreReturnValueOfTypeIDisposable.DiagnosticId);
 
@@ -40,13 +43,22 @@ namespace Gu.Analyzers
                     var statement = node.FirstAncestorOrSelf<ExpressionStatementSyntax>();
                     if (statement != null)
                     {
+                        var usesUnderscoreNames = statement.UsesUnderscoreNames(semanticModel, context.CancellationToken);
                         if (TryGetField(statement, semanticModel, context.CancellationToken, out IFieldSymbol field))
                         {
-                            var usesUnderscoreNames = statement.UsesUnderscoreNames(semanticModel, context.CancellationToken);
                             context.RegisterCodeFix(
                                 CodeAction.Create(
                                     "Add to CompositeDisposable.",
                                     _ => ApplyAddToCompositeDisposableFixAsync(context, statement, field, usesUnderscoreNames),
+                                    nameof(AddToCompositeDisposableCodeFixProvider)),
+                                diagnostic);
+                        }
+                        else
+                        {
+                            context.RegisterCodeFix(
+                                CodeAction.Create(
+                                    "Add to new CompositeDisposable.",
+                                    _ => ApplyAddToCompositeDisposableFixAsync(context, statement, usesUnderscoreNames),
                                     nameof(AddToCompositeDisposableCodeFixProvider)),
                                 diagnostic);
                         }
@@ -64,8 +76,7 @@ namespace Gu.Analyzers
                                                        .Generator.MemberAccessExpression(
                                                            SyntaxFactory.IdentifierName(field.Name),
                                                            "Add")
-                                                   : (MemberAccessExpressionSyntax)editor
-                                                       .Generator.MemberAccessExpression(
+                                                   : (MemberAccessExpressionSyntax)editor.Generator.MemberAccessExpression(
                                                            editor.Generator.MemberAccessExpression(
                                                                SyntaxFactory.ThisExpression(),
                                                                SyntaxFactory.IdentifierName(field.Name)),
@@ -77,6 +88,61 @@ namespace Gu.Analyzers
                     (InvocationExpressionSyntax)editor.Generator.InvocationExpression(
                         memberAccessExpressionSyntax,
                         statement.Expression)));
+            return editor.GetChangedDocument();
+        }
+
+        private static async Task<Document> ApplyAddToCompositeDisposableFixAsync(CodeFixContext context, ExpressionStatementSyntax statement, bool usesUnderscoreNames)
+        {
+            var editor = await DocumentEditor.CreateAsync(context.Document)
+                                             .ConfigureAwait(false);
+            var name = usesUnderscoreNames
+                           ? "_disposable"
+                           : "disposable";
+            var containingType = statement.FirstAncestor<TypeDeclarationSyntax>();
+            var declaredSymbol = editor.SemanticModel.GetDeclaredSymbol(containingType);
+            while (declaredSymbol.MemberNames.Contains(name))
+            {
+                name += "_";
+            }
+
+            var newField = (FieldDeclarationSyntax)editor.Generator.FieldDeclaration(
+                name,
+                accessibility: Accessibility.Private,
+                modifiers: DeclarationModifiers.ReadOnly,
+                type: CompositeDisposableType);
+            var members = containingType.Members;
+            if (members.TryGetFirst(x => x is FieldDeclarationSyntax, out MemberDeclarationSyntax field))
+            {
+                editor.InsertBefore(field, new[] { newField });
+            }
+            else if (members.TryGetFirst(out field))
+            {
+                editor.InsertBefore(field, new[] { newField });
+            }
+            else
+            {
+                editor.AddMember(containingType, newField);
+            }
+
+            var fieldAccess = usesUnderscoreNames
+                                  ? SyntaxFactory.IdentifierName(name)
+                                  : SyntaxFactory.ParseExpression($"this.{name}");
+
+            editor.ReplaceNode(
+                statement,
+                SyntaxFactory.ExpressionStatement(
+                                 (ExpressionSyntax)editor.Generator.AssignmentStatement(
+                                     fieldAccess,
+                                     ((ObjectCreationExpressionSyntax)editor
+                                         .Generator.ObjectCreationExpression(
+                                             CompositeDisposableType))
+                                     .WithInitializer(
+                                         SyntaxFactory.InitializerExpression(
+                                             SyntaxKind.CollectionInitializerExpression,
+                                             SyntaxFactory.SingletonSeparatedList(
+                                                 statement.Expression)))))
+                             .WithLeadingTrivia(SyntaxFactory.ElasticMarker)
+                             .WithTrailingTrivia(SyntaxFactory.ElasticMarker));
             return editor.GetChangedDocument();
         }
 
--- Gu.Analyzers.Test/GU0033DontIgnoreReturnValueOfTypeIDisposableTests/CodeFixAddToCompositeDisposable.cs
+++ Gu.Analyzers.Test/GU0033DontIgnoreReturnValueOfTypeIDisposableTests/CodeFixAddToCompositeDisposable.cs
@@ -6,7 +6,103 @@
     internal class CodeFixAddToCompositeDisposable : CodeFixVerifier<GU0033DontIgnoreReturnValueOfTypeIDisposable, AddToCompositeDisposableCodeFixProvider>
     {
         [Test]
-        public async Task AddIgnoredReturnValueToCompositeDisposableCtor()
+        public async Task AddIgnoredReturnValueToCreatedCompositeDisposableCtor()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+    using System.Reactive.Disposables;
+
+    internal sealed class Foo
+    {
+        internal Foo()
+        {
+            ↓File.OpenRead(string.Empty);
+        }
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Don't ignore returnvalue of type IDisposable.");
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+    using System.Reactive.Disposables;
+
+    internal sealed class Foo
+    {
+        private readonly CompositeDisposable disposable;
+
+        internal Foo()
+        {
+            this.disposable = new CompositeDisposable() { File.OpenRead(string.Empty) };
+        }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task AddIgnoredReturnValueToCreatedCompositeDisposableInitializer()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+    using System.Reactive.Disposables;
+
+    internal sealed class Foo
+    {
+        private readonly CompositeDisposable disposable;
+
+        internal Foo()
+        {
+            this.disposable = new CompositeDisposable
+            {
+                File.OpenRead(string.Empty)
+            };
+            ↓File.OpenRead(string.Empty);
+        }
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Don't ignore returnvalue of type IDisposable.");
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+    using System.Reactive.Disposables;
+
+    internal sealed class Foo
+    {
+        private readonly CompositeDisposable disposable;
+
+        internal Foo()
+        {
+            this.disposable = new CompositeDisposable
+            {
+                File.OpenRead(string.Empty),
+                File.OpenRead(string.Empty)
+            };
+        }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task AddIgnoredReturnValueToExistingCompositeDisposableCtor()
         {
             var testCode = @"
 namespace RoslynSandbox
@@ -51,7 +147,7 @@ namespace RoslynSandbox
         }
 
         [Test]
-        public async Task AddIgnoredReturnValueToCompositeDisposableCtorUnderscore()
+        public async Task AddIgnoredReturnValueToExistingCompositeDisposableCtorUnderscore()
         {
             var testCode = @"
 namespace RoslynSandbox
@@ -95,7 +191,7 @@ namespace RoslynSandbox
             await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
         }
 
-        [Test, Explicit]
+        [Test]
         public async Task AddIgnoredReturnValueToCompositeDisposableInitializer()
         {
             var testCode = @"
@@ -111,7 +207,7 @@ namespace RoslynSandbox
 
         internal Foo()
         {
-            disposable = new CompositeDisposable();
+            this.disposable = new CompositeDisposable();
             ↓File.OpenRead(string.Empty);
         }
     }
@@ -134,10 +230,8 @@ namespace RoslynSandbox
 
         internal Foo()
         {
-            disposable = new CompositeDisposable
-            {
-                File.OpenRead(string.Empty)
-            };
+            this.disposable = new CompositeDisposable();
+            this.disposable.Add(File.OpenRead(string.Empty));
         }
     }
 }";
