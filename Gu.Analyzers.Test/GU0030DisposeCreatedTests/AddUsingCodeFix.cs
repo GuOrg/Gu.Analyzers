1fbe8240e433581b76d7beb5b313f121375f27dd
Johan Larsson <johan_gavle1@hotmail.com>
2017-05-01 16:08:21 +0200

    Added failing test where we generate poor formatting.

--- Gu.Analyzers.Test/GU0030DisposeCreatedTests/AddUsingCodeFix.cs
+++ Gu.Analyzers.Test/GU0030DisposeCreatedTests/AddUsingCodeFix.cs
@@ -47,5 +47,59 @@ namespace RoslynSandbox
 }";
             await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
         }
+
+        [Explicit("Poor formatting.")]
+        [Test]
+        public async Task AddUsingForLocalManyStatements()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class Foo
+    {
+        public void Meh()
+        {
+            ↓var stream = File.OpenRead(string.Empty);
+            var a = 1;
+            var b = 1;
+            if (a == b)
+            {
+                var c = 2;
+            }
+        }
+    }
+}";
+            var expected = this.CSharpDiagnostic()
+                               .WithLocationIndicated(ref testCode)
+                               .WithMessage("Dispose created.");
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class Foo
+    {
+        public void Meh()
+        {
+            using (var stream = File.OpenRead(string.Empty))
+            {
+                var a = 1;
+                var b = 1;
+                if (a == b)
+                {
+                    var c = 2;
+                }
+            }
+        }
+    }
+}";
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
     }
 }
