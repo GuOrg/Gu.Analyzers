51ba124f84f9f3a7ae1decad0d6599432d7cad3d
Andrew <37260562+andrewtechnology@users.noreply.github.com>
2018-03-18 01:58:10 -0700

    New analyzer: Check that [TestCase] arguments match parameter types, fix #148

---
+++ Gu.Analyzers.Analyzers/GU0083TestCaseAttributeMismatchMethod.cs
@@ -0,0 +1,19 @@
+﻿namespace Gu.Analyzers
+{
+    using Microsoft.CodeAnalysis;
+
+    internal class GU0083TestCaseAttributeMismatchMethod
+    {
+        public const string DiagnosticId = "GU0083";
+
+        internal static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            id: DiagnosticId,
+            title: "TestCase Arguments Mismatch Method Parameters",
+            messageFormat: "TestCase Arguments Mismatch Method Parameters {0}.",
+            category: AnalyzerCategory.Correctness,
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: AnalyzerConstants.EnabledByDefault,
+            description: "TestCase Mismatches Method Parameters",
+            helpLinkUri: HelpLink.ForId(DiagnosticId));
+    }
+}
---
+++ Gu.Analyzers.Test/GU0083TestCaseAttributeMismatchMethodTests/Diagnostics.cs
@@ -0,0 +1,68 @@
+﻿namespace Gu.Analyzers.Test.GU0083TestCaseAttributeMismatchMethodTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    internal class Diagnostics
+    {
+        private static readonly TestMethodAnalyzer Analyzer = new TestMethodAnalyzer();
+        private static readonly ExpectedDiagnostic ExpectedDiagnostic = ExpectedDiagnostic.Create("GU0083");
+
+        [Test]
+        public void TestCaseAttribute_IfSingleParameterIsWrong()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class FooTests
+    {
+        [TestCase(↓1)]
+        public void Test(string str)
+        {
+        }
+    }
+}";
+            AnalyzerAssert.Diagnostics(Analyzer, ExpectedDiagnostic, testCode);
+        }
+
+        [Test]
+        public void TestCaseAttribute_IfMultipleParametersAreWrong()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class FooTests
+    {
+        [TestCase(1, ↓2)]
+        public void Test(int i, string str)
+        {
+        }
+    }
+}";
+            AnalyzerAssert.Diagnostics(Analyzer, ExpectedDiagnostic, testCode);
+        }
+
+        [Test]
+        public void TestCaseAttribute_IfTestCaseArgumentIsNull()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class FooTests
+    {
+        [TestCase(null)]
+        public void Test(object obj)
+        {
+        }
+    }
+}";
+            AnalyzerAssert.Diagnostics(Analyzer, ExpectedDiagnostic, testCode);
+        }
+    }
+}
