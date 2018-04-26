66b542cfc9884510b76a4890a07f88673acc3b62
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-26 15:44:15 +0200

    Remove unused.

--- Gu.Analyzers.Analyzers/Helpers/ResultExt.cs
+++
@@ -1,7 +0,0 @@
-namespace Gu.Analyzers
-{
-    internal static class ResultExt
-    {
-        internal static bool IsEither(this Result result, Result first, Result other) => result == first || result == other;
-    }
-}
