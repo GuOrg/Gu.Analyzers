4d5df55b1e48250e1c6af10206957c6b8ce7ce73
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-08 09:56:38 +0100

    Fix links.
    Fix #261.

--- Gu.Analyzers.Test/DocumentationTests.cs
+++ Gu.Analyzers.Test/DocumentationTests.cs
@@ -325,7 +325,7 @@ Or put this at the top of the file to disable all instances.
 
             public string Name { get; }
 
-            public string Uri => "https://github.com/DotNetAnalyzers/Gu.Analyzers/blob/master" + this.Name.Substring(SolutionDirectory.FullName.Length)
+            public string Uri => "https://github.com/GuOrg/Gu.Analyzers/blob/master" + this.Name.Substring(SolutionDirectory.FullName.Length)
                                                                                                              .Replace("\\", "/", StringComparison.Ordinal);
 
             public static CodeFile Find(Type type)
