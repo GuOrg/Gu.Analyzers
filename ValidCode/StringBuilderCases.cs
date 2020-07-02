57945967aa426deb18887d58da511a27c6a0ee45
JohanLarsson <johan_gavle1@hotmail.com>
2020-07-02 10:43:25 +0200

    More valid code.

---
+++ ValidCode/StringBuilderCases.cs
@@ -0,0 +1,13 @@
+﻿namespace ValidCode
+{
+    using System.Text;
+
+    class StringBuilderCases
+    {
+        public void M(string typeName)
+        {
+            var errorBuilder = new StringBuilder();
+            errorBuilder.AppendLine($"Found more than one match for {typeName}");
+        }
+    }
+}
