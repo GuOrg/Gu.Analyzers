99430a2159d8b21f890004483a0af5d9c5dbb222
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-25 17:31:36 +0100

    No repro.

---
+++ ValidCode/ReturnValue/Ext.cs
@@ -0,0 +1,27 @@
+﻿namespace ValidCode.ReturnValue
+{
+    using System;
+
+    internal static class Ext
+    {
+        internal static C M1Ext(this C c, Type t) => c;
+        internal static C M2Ext(this C c, Type t)
+        {
+            return c;
+        }
+
+        internal static void M1()
+        {
+            using var c1 = new C().M1Ext(typeof(C));
+            using var c2 = new C();
+            c2.M1Ext(typeof(C));
+        }
+
+        internal static void M2()
+        {
+            using var c1 = new C().M2Ext(typeof(C));
+            using var c2 = new C();
+            c2.M2Ext(typeof(C));
+        }
+    }
+}
