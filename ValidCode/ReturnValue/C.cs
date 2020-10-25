a8a579c8a6118646411417b1267fe615623768aa
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-25 09:48:41 +0100

    No repro.

---
+++ ValidCode/ReturnValue/C.cs
@@ -0,0 +1,18 @@
+﻿namespace ValidCode.ReturnValue
+{
+    using System;
+
+    internal class C : IDisposable
+    {
+        internal C M1(Type t) => this;
+        internal C M2(Type t)
+        {
+            this.M1(t);
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ ValidCode/ReturnValue/C1.cs
@@ -0,0 +1,19 @@
+﻿namespace ValidCode.ReturnValue
+{
+    internal class C1
+    {
+        internal void M1()
+        {
+            using var c1 = new C().M1(typeof(C));
+            using var c2 = new C();
+            c2.M1(typeof(C));
+        }
+
+        internal void M2()
+        {
+            using var c1 = new C().M2(typeof(C));
+            using var c2 = new C();
+            c2.M2(typeof(C));
+        }
+    }
+}
