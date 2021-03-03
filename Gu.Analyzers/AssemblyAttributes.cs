8b4dd53424e35d6381e347b11b86fda233e6a026
JohanLarsson <johan_gavle1@hotmail.com>
2021-03-03 14:17:38 +0100

    [assembly: SuppressMessage("Roslyn.HostError", "CS8033", Justification = "Analyzer getting it wrong.")]

--- Gu.Analyzers/AssemblyAttributes.cs
+++ Gu.Analyzers/AssemblyAttributes.cs
@@ -1,3 +1,5 @@
-using System;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 [assembly: CLSCompliant(false)]
+[assembly: SuppressMessage("Roslyn.HostError", "CS8033", Justification = "Analyzer getting it wrong.")
