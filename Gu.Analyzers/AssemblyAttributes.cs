c0acdc1fce5e6cda4205f863e692b2b4acabbba1
Johan Larsson <johan_gavle1@hotmail.com>
2021-03-03 16:43:15 +0100

    <NoWarn>CS8033</NoWarn>

--- Gu.Analyzers/AssemblyAttributes.cs
+++ Gu.Analyzers/AssemblyAttributes.cs
@@ -1,5 +1,3 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 [assembly: CLSCompliant(false)]
-[assembly: SuppressMessage("Build", "CS8033", Justification = "Analyzer getting it wrong.")]
